cda69fba79adb76b074760b2fa1b426b3d31a24a
fisker <lionkay@gmail.com>
2018-04-10 10:00:31 +0800

    revert changes to `interval check` and `catch`

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 'use strict';
 const nonBooleanError = new TypeError('Expected condition to return a boolean');
 
-module.exports = (condition, interval) => {
+module.exports = (condition, interval) => new Promise((resolve, reject) => {
 	interval = typeof interval === 'number' ? interval : 20;
 
-	return new Promise((resolve, reject) => {
-		const check = () => {
-			Promise.resolve(condition()).then(val => {
-				if (val === true) {
-					resolve();
-				} else if (val === false) {
-					setTimeout(check, interval);
-				} else {
-					throw nonBooleanError;
-				}
-			}, reject);
-		};
+	const check = () => {
+		Promise.resolve(condition()).then(val => {
+			if (val === true) {
+				resolve();
+			} else if (val === false) {
+				setTimeout(check, interval);
+			} else {
+				throw nonBooleanError;
+			}
+		}).catch(reject);
+	};
 
-		check();
-	});
-};
+	check();
+});
