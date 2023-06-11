f8a235d425c44e8010a28f037172c699a3a27a90
ronami <ronena@wix.com>
2023-06-11 13:32:13 +0300

    add tsd tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,10 +1,13 @@
 import {expectType} from 'tsd';
 import pWaitFor from './index.js';
 
+const controller = new AbortController();
+
 expectType<Promise<void>>(pWaitFor(() => false));
 expectType<Promise<void>>(pWaitFor(async () => false));
 expectType<Promise<void>>(pWaitFor(() => true, {interval: 1}));
 expectType<Promise<void>>(pWaitFor(() => true, {timeout: 1}));
+expectType<Promise<void>>(pWaitFor(() => true, {signal: controller.signal}));
 expectType<Promise<void>>(pWaitFor(() => true, {before: false}));
 expectType<Promise<number>>(pWaitFor(() => pWaitFor.resolveWith(1)));
 expectType<Promise<number>>(pWaitFor(() => pWaitFor.resolveWith(1)));
